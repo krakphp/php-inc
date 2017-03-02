de16ccb52b7cd6aa116b24de009542df23a924e8
RJ Garcia <rj@bighead.net>
2017-03-02 06:31:11 -0800

    Adding output option
    Signed-off-by: RJ Garcia <rj@bighead.net>

--- src/Command/GenerateCommand.php
+++ src/Command/GenerateCommand.php
@@ -3,7 +3,8 @@
 namespace Krak\PhpInc\Command;
 
 use Krak\PhpInc,
-    Symfony\Component\Console;
+    Symfony\Component\Console,
+    Webmozart\PathUtil\Path;
 
 class GenerateCommand extends Console\Command\Command
 {
@@ -14,18 +15,34 @@ class GenerateCommand extends Console\Command\Command
                 'path',
                 Console\Input\InputArgument::REQUIRED,
                 'The path of the source files to scan for'
+            )
+            ->addOption(
+                'output',
+                'o',
+                Console\Input\InputOption::VALUE_REQUIRED,
+                'The output file to write too. This file will be exluded in inc matching'
             );
     }
 
     protected function execute(Console\Input\InputInterface $input, Console\Output\OutputInterface $output) {
-        $match = PhpInc\andMatch([
+        $o = $input->getOption('output');
+        $o = $o ? Path::makeAbsolute($o, getcwd()) : null;
+        $match = PhpInc\andMatch(array_filter([
             PhpInc\extMatch(['php']),
             PhpInc\lowerCaseMatch(),
             PhpInc\excludePathMatch('/.*\/Resources\/.*/'),
-        ]);
+            $o ? PhpInc\excludePathMatch("@^$o$@") : null
+        ]));
+
         $scan = PhpInc\scanSrc($match);
         $gen = PhpInc\genIncFile();
         $phpinc = PhpInc\phpInc($scan, $gen);
-        $output->write($phpinc($input->getArgument('path')));
+        $inc_file = $phpinc($input->getArgument('path'));
+
+        if ($o) {
+            file_put_contents($o, $inc_file);
+        } else {
+            $output->write($inc_file);
+        }
     }
 }
